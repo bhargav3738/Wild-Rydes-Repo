334808556234a4b30ad38ebfb299056550192f7c
bhargav3738 <nimbalkarbhargav19@gmail.com>
2025-03-29 17:08:46 -0400

    updated database

--- flightbooking/settings.py
+++ flightbooking/settings.py
@@ -101,23 +101,23 @@ WSGI_APPLICATION = "flightbooking.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'flight_service_booking_db',
-#         'USER': 'postgres',
-#         'PASSWORD': 'password',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     },
-
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_db',
+        'USER': 'django_user',
+        'PASSWORD': 'L2)00+9Ic[ae-r*K',
+        'HOST': 'django-postgres.c9eqysqgqbq5.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
+    },
+
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
